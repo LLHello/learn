385a6ad79fc19e22c13c8df2b59fa75f12b617b6
LLHello <l2352707455@outlook.com>
2024-12-13 11:59:17 +0800

    hot-fix test commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot-fix git3");
         System.out.println("hello hot-fix git4");
         System.out.println("hello world!");
+        System.out.println("fot-fix test!");
     }
 }
